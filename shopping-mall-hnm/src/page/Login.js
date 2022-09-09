2385eac87bd6cbff7b5bea457c26fe48a70e0149
jinnny92 <jinnny22@naver.com>
2022-09-10 03:32:19 +0900

    상품상세페이지

--- shopping-mall-hnm/src/page/Login.js
+++ shopping-mall-hnm/src/page/Login.js
@@ -10,22 +10,16 @@ const Login = ({ setAuthenticate }) => {
     navigate("/");
   };
   return (
-    <Container>
-      <Form onSubmit={(event) => loginUser(event)}>
+    <Container className="login-area">
+      <Form className="login-form" onSubmit={loginUser}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
         <Button variant="danger" type="submit">
           Login
--- shopping-mall-hnm/src/page/ProductDetail.js
+++ shopping-mall-hnm/src/page/ProductDetail.js
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
+import { Col, Container, Row, Dropdown, Button } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
-  return <div></div>;
+  const { id } = useParams();
+  const [product, setProduct] = useState("");
+  const getProductDetail = async () => {
+    let url = `http://localhost:5000/products/${id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+
+    setProduct(data);
+  };
+  useEffect(() => {
+    getProductDetail();
+  }, []);
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col className="product-img">
+            <img src={product.img} />
+          </Col>
+          <Col>
+            <div className="product-info">{product.title}</div>
+            <div className="product-info">₩ {product.price}</div>
+            <div className="choice">
+              {product.choice ? "Conscious choice" : ""}
+            </div>
+            <Dropdown className="drop-down">
+              <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
+                사이즈 선택
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>{product.size}</Dropdown.Menu>
+            </Dropdown>
+            <Button variant="dark" className="add-button">
+              추가
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default ProductDetail;
