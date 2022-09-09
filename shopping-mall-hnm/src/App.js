16287bc6e3556c88bc6d7c3a9fc977299b2fbde4
jinnny92 <jinnny22@naver.com>
2022-09-09 21:19:02 +0900

    json-server

---
+++ shopping-mall-hnm/src/App.js
@@ -0,0 +1,32 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Login from "./page/Login";
+import ProductAll from "./page/ProductAll";
+import ProductDetail from "./page/ProductDetail";
+
+//1. 전체 상품 페이지, 로그인. 상품상세페이지
+//1-1. 네비게이션바 만들기
+//2. 전체 상품 페이지에서는 전체 상품을 볼 수 있다
+//3. 로그인 버튼을 누르면 로그인 페이지가 나온다
+//3. 상품 디테일을 눌렀으나, 로그인이 안되어있을 경우에는 로그인페이지가 먼저 나온다
+//4. 로그인이 되어있을 경우에는 상품 디테일 페이지를 볼 수 있다
+//5. 로그아웃 버튼을 클릭하면 로그아웃이 된다
+//5. 로그아웃이 되면 상품 디테일 페이지를 볼 수 없다, 다시 로그인페이지가 보인다
+//6. 로그인을 하면 로그아웃이 보이고 로그아웃을 하면 로그인이 보인다
+//7. 상품을 검색할 수 있다.
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<ProductAll />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ shopping-mall-hnm/src/component/Navbar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+const Navbar = () => {
+  const menuList = [
+    "여성",
+    "Divided",
+    "남성",
+    "신생아/유아",
+    "아동",
+    "H&M Home",
+    "Sale",
+    "지속가능성",
+  ];
+  return (
+    <div>
+      <div className="login-button">
+        <FontAwesomeIcon icon={faUser} />
+        <div>로그인</div>
+      </div>
+      <div className="nav-section">
+        <img
+          width={300}
+          src="https://cdn.cookielaw.org/logos/6e0ffeab-df84-4fee-b293-9e6498bfa887/697b276d-c669-4d88-b115-dd9e6cae3fae/28a9d5ed-4776-4fe0-9e19-007e8ed817a9/709px-H&M-Logo.svg.png"
+        />
+      </div>
+      <div className="menu-area">
+        <ul className="menu-list">
+          {menuList.map((menu) => (
+            <li>{menu}</li>
+          ))}
+        </ul>
+
+        <div className="search-box">
+          <FontAwesomeIcon icon={faSearch} />
+          <input type="text" placeholder="제품검색" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
