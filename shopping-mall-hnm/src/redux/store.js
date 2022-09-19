ac93085c8fcb269ee563487fd55802404db0458f
jinnny92 <jinnny22@naver.com>
2022-09-19 23:15:31 +0900

    이덕스Devtool

--- shopping-mall-hnm/src/redux/store.js
+++ shopping-mall-hnm/src/redux/store.js
@@ -1,7 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-let store = createStore(rootReducer, applyMiddleware(thunk));
+let store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
