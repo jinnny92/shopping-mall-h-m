ce42607a3aab5ea5f33a808ee4f08e91d8774c88
jinnny92 <jinnny22@naver.com>
2022-09-20 02:05:34 +0900

    제이슨변경

--- shopping-mall-hnm/src/redux/actions/productAction.js
+++ shopping-mall-hnm/src/redux/actions/productAction.js
@@ -1,7 +1,7 @@
 import { productActions } from "../reducers/productReducer";
 function getProducts(searchQuery) {
   return async (dispatch, getState) => {
-    let url = `http://localhost:5000/products?q=${searchQuery}`;
+    let url = `https://my-json-server.typicode.com/jinnny92/shopping-mall-h-m/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
     //dispatch({ type: "GET_PRODUCT_SUCCESS", payload: { data } });
